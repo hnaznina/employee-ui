c73f16dd9a8cddb8b6346b48947f0d5b2241008e
hnaznina <hoque.naznina@gmail.com>
2018-12-14 21:23:43 -0600

    gradel-init

---
+++ src/main/java/com/example/employeeui/EmployeeUiApplication.java
@@ -0,0 +1,14 @@
+package com.example.employeeui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeUiApplication.class, args);
+	}
+
+}
+
